5fca38d539b2b6616589fff1fa1538873c9320d1
Eduardo Souza <edcampossouza@gmail.com>
2022-10-27 10:17:37 -0300

    funcionalidade : login

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const periodoRecarregar = 2;
 const periodoAvisoOnline = 5;
 
+const URL_LOGIN = "https://mock-api.driven.com.br/api/v6/uol/participants";
 const URL_MENSAGENS = "https://mock-api.driven.com.br/api/v6/uol/messages";
 
 //carregar mensagens
@@ -17,8 +18,8 @@ function carregaMensagens() {
         </div>
         `
           : msg.type === "private_message"
-          ? `<div class="message-box">
-          <span class="time">(${msg.time})</span>
+          ? `<div class="message-box private">
+          <span class="time">(${msg.time}) </span>
           <span class="username">${msg.from} </span>
           reservadamente fala para <span class="username">${msg.to} </span>
           ${msg.text}
@@ -36,11 +37,26 @@ function carregaMensagens() {
   });
 }
 
+//pergunta nome e entra na sala
+function login() {
+  let nome = null;
+  nome = prompt("Digite seu nome...");
+  //login
+  axios
+    .post(URL_LOGIN, { name: nome })
+    .then(function response(resposta) {
+      setInterval(avisaStatusOnline, 1000 * periodoAvisoOnline);
+      setInterval(carregaMensagens, 1000 * periodoRecarregar);
+      return;
+    })
+    .catch(function (erro) {
+      alert("Nome indisponível");
+      login();
+    });
+}
 //avisa ao servidor que o usuario esta online
 function avisaStatusOnline() {
   console.log("estou online!");
 }
 
-carregaMensagens();
-setInterval(carregaMensagens, 1000 * periodoRecarregar);
-setInterval(avisaStatusOnline, 1000 * periodoAvisoOnline);
+login();
